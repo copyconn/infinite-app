9726ba9b281862d4e4603af146fad39e65037c3f
copyconn <100408800+copyconn@users.noreply.github.com>
2023-05-30 20:20:07 +0300

    feat: infinite loading

--- client/src/modules/Dashboard/Tables/Tables.jsx
+++ client/src/modules/Dashboard/Tables/Tables.jsx
@@ -1,30 +1,67 @@
-import React, { useContext, useState } from "react";
+import React, { useCallback, useContext } from "react";
 import { AgGridReact } from "ag-grid-react";
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { Button } from '@mantine/core';
 
-import { columnsAuthor } from "./utils";
+import { columnsAuthor, columnsBook } from "./utils";
 
 import { dataContext } from "../../../App";
+import { getAuthors } from "../../../api";
 
 export const Tables = () => {
     const data = useContext(dataContext)
-    const [rowDataAuthor] = useState(data.authors)
-    const [columnDefsAuthor] = useState(columnsAuthor)
 
+    const books = data.books.map((bookRow) => {
+        return { ...bookRow, createdAt: bookRow.createdAt.split('-').reverse().join('.') }
+    })
+
+    const onGridReady = useCallback((params) => {
+        const dataSource = {
+            rowCount: undefined,
+            getRows: (params) => {
+                getAuthors(params.endRow - params.startRow, params.startRow)
+                    .then((result) => {
+                        const authors = result.data.rows.map((authorRow) => {
+                            return { ...authorRow, created_at: authorRow.created_at.split('-').reverse().join('.') }
+                        })
+                        params.successCallback(authors, result.data.count)
+                    })
+
+            },
+        };
+        params.api.setDatasource(dataSource);
+    }, [])
     return (
         <div>
             <div>
                 <h3>Таблица "Автор"</h3>
-                <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-                    <AgGridReact rowData={rowDataAuthor} columnDefs={columnDefsAuthor}></AgGridReact>
+
+                <div className="ag-theme-alpine" style={{ height: 400, width: 800 }}>
+                    <AgGridReact
+                        columnDefs={columnsAuthor}
+                        rowModelType={'infinite'}
+                        onGridReady={onGridReady}
+                        cacheBlockSize={10}
+                    ></AgGridReact>
                 </div>
             </div>
 
             <div>
-                <h3>Таблица "Автор"</h3>
-                <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-                    <AgGridReact rowData={rowDataAuthor} columnDefs={columnDefsAuthor}></AgGridReact>
+                <h3>Таблица "Книги"</h3>
+
+                <div style={{ display: 'flex', gap: 10 }}>
+                    <Button variant={"outline"}>Добавить запись</Button>
+                    <Button variant={"outline"}>Изменить запись</Button>
+                    <Button variant={"outline"}>Удалить запись</Button>
+                </div>
+
+                <div className="ag-theme-alpine" style={{ height: 400, width: 800, marginTop: 10 }}>
+                    <AgGridReact
+                        rowData={books}
+                        columnDefs={columnsBook}
+                        rowSelection={'single'}
+                    ></AgGridReact>
                 </div>
             </div>
         </div>
